aa858ba8f2d456c5b6d2f7147067ef1144419a3c
Derek Thurn <derek@fake.email>
2024-04-03 09:57:31 -0700

    initial commit

---
+++ src/main/src/app.rs
@@ -0,0 +1,107 @@
+// Copyright © Dungeon of the Diamond Queen 2024-present
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use color_eyre::eyre::{bail, WrapErr};
+use color_eyre::Result;
+use crossterm::event;
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::prelude::*;
+use ratatui::symbols::border;
+use ratatui::widgets::block::{Position, Title};
+use ratatui::widgets::{Block, Borders, Paragraph};
+use ratatui::Frame;
+
+use crate::tui::Tui;
+
+#[derive(Debug, Default)]
+pub struct App {
+    counter: u8,
+    exit: bool,
+}
+
+impl App {
+    /// runs the application's main loop until the user quits
+    pub fn run(&mut self, tui: &mut Tui) -> Result<()> {
+        while !self.exit {
+            tui.draw(|frame| self.render_frame(frame))?;
+            self.handle_events().wrap_err("handle events failed")?;
+        }
+        Ok(())
+    }
+
+    fn render_frame(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.size());
+    }
+
+    /// updates the application's state based on user input
+    fn handle_events(&mut self) -> Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => self
+                .handle_key_event(key_event)
+                .wrap_err_with(|| format!("handling key event failed:\n{key_event:#?}")),
+            _ => Ok(()),
+        }
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<()> {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter()?,
+            KeyCode::Right => self.increment_counter()?,
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn decrement_counter(&mut self) -> Result<()> {
+        self.counter -= 1;
+        Ok(())
+    }
+
+    fn increment_counter(&mut self) -> Result<()> {
+        self.counter += 1;
+        if self.counter > 2 {
+            bail!("counter overflow");
+        }
+        Ok(())
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Title::from(" Counter App Tutorial ".bold());
+        let instructions = Title::from(Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]));
+        let block = Block::default()
+            .title(title.alignment(Alignment::Center))
+            .title(instructions.alignment(Alignment::Center).position(Position::Bottom))
+            .borders(Borders::ALL)
+            .border_set(border::THICK);
+
+        let counter_text =
+            Text::from(vec![Line::from(vec!["Value: ".into(), self.counter.to_string().yellow()])]);
+
+        Paragraph::new(counter_text).centered().block(block).render(area, buf);
+    }
+}
---
+++ src/main/src/main.rs
@@ -0,0 +1,37 @@
+// Copyright © Dungeon of the Diamond Queen 2024-present
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use clap::Parser;
+use cli::Cli;
+use color_eyre::eyre::Result;
+use tracing::info;
+
+use crate::app::App;
+
+pub mod app;
+pub mod cli;
+pub mod tui;
+pub mod utils;
+
+fn main() -> Result<()> {
+    utils::initialize_logging()?;
+    utils::initialize_panic_handler()?;
+    Cli::parse();
+    let mut tui = tui::enter()?;
+    let commit = env!("VERGEN_GIT_SHA");
+    info!(commit, "Starting game");
+    App::default().run(&mut tui)?;
+    tui::exit()?;
+    Ok(())
+}
